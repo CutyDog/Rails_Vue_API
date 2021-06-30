908029f209ad3085134444f6fe10375bab5e359d
CutyDog <silver.soul.818@gmail.com>
2021-07-01 05:17:49 +0900

    Vue setup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <PostForm />
+    <PostList />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostForm from "./components/PostForm.vue";
+import PostList from "./components/PostList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    PostForm,
+    PostList
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
